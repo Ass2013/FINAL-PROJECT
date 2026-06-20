3fbaa52d4c7814f39cd9cc1a831feca251bfe2e2
Anton <ant.ser.sol@gmail.com>
2026-06-20 12:52:06 +0200

    Update logic.py

--- logic.py
+++ logic.py
@@ -6,8 +6,8 @@ conn = sqlite3.connect("support_bot.db")
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS Questions (
-    question_id INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS Cathegories (
+    cathegory_id INTEGER PRIMARY KEY,
     problem TEXT NOT NULL
 )
 """)
@@ -16,25 +16,19 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS "Users Questions" (
     user_id INTEGER NOT NULL,
     username TEXT NOT NULL,
-    question_id INTEGER NOT NULL,
+    cathegory_id INTEGER NOT NULL,
     question TEXT NOT NULL,
 
-    PRIMARY KEY (user_id, question_id),
+    PRIMARY KEY (user_id, cathegory_id),
 
-    FOREIGN KEY (question_id)
-        REFERENCES Questions(question_id)
+    FOREIGN KEY (cathegory_id)
+        REFERENCES Cathegories(cathegory_id)
 )
 """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS "Quick Questions" (
-    question TEXT PRIMARY KEY,
-    answer TEXT NOT NULL
-)
-""")
 
 cursor.executemany(
-    "INSERT OR IGNORE INTO Questions (question_id, problem) VALUES (?, ?)",
+    "INSERT OR IGNORE INTO Cathegories (cathegory_id, problem) VALUES (?, ?)",
     [
         (1, "Transport"),
         (2, "Tech problem"),
@@ -43,21 +37,20 @@ cursor.executemany(
     ]
 )
 
-cursor.executemany(
-    "INSERT OR IGNORE INTO 'Quick Questions' (question, answer) VALUES (?, ?)",
-    [
-        ("Как оформить заказ?", "Для оформления заказа, пожалуйста, выберите интересующий вас товар и нажмите кнопку \"Добавить в корзину\", затем перейдите в корзину и следуйте инструкциям для завершения покупки."),
-        ("Как узнать статус моего заказа?", "Вы можете узнать статус вашего заказа, войдя в свой аккаунт на нашем сайте и перейдя в раздел \"Мои заказы\". Там будет указан текущий статус вашего заказа."),
-        ("Как отменить заказ?", "Если вы хотите отменить заказ, пожалуйста, свяжитесь с нашей службой поддержки как можно скорее. Мы постараемся помочь вам с отменой заказа до его отправки."),
-        ("Что делать, если товар пришел поврежденным?", "При получении поврежденного товара, пожалуйста, сразу свяжитесь с нашей службой поддержки и предоставьте фотографии повреждений. Мы поможем вам с обменом или возвратом товара."),
-        ("Как связаться с вашей технической поддержкой?", "Вы можете связаться с нашей технической поддержкой через телефон на нашем сайте или написать нам в чат-бота."),
-        ("Как узнать информацию о доставке?", "Информацию о доставке вы можете найти на странице оформления заказа на нашем сайте. Там указаны доступные способы доставки и сроки."),
-        ("Другое", "-")
-    ]
-)
 
 
 conn.commit()
 conn.close()
+
+def save_question(message, cathegory_id):
+    conn = sqlite3.connect("support_bot.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO "Users Questions" (user_id, username, cathegory_id, question)
+        VALUES (?, ?, ?, ?)
+    """, (message.from_user.id, message.from_user.username, cathegory_id, message.text))
+    # bot.send_message(call.message.chat.id, "✅ Ваша проблема сохранена. Мы скоро ответим.")
+    conn.commit()
+    conn.close()
 
 print("support_bot.db created successfully!")
